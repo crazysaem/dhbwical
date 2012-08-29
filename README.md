fd5d9b4d3d4ec5ec697a6693a223f1fbcec9e9ad
Samuel Schneider <crazysaem@googlemail.com>
2012-08-29 18:12:57 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-dhbwical
+DHBW ical converter
 ========
 
-Converts the DHBW Calendar into an ical file
+Converts the DHBW Calendar into an ical file (hardcoded for tai10abc)
+
+Requirements:
+
+Python http://www.python.org/
+Beautiful Soap http://www.crummy.com/software/BeautifulSoup/
---
+++ tai10abc_to_ical.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import urllib
+
+def convertstring(str):
+	str = str.replace(u"\xe4", "a")
+	str = str.replace(u"\xc4", "A")
+	str = str.replace(u"\xf6", "o")
+	str = str.replace(u"\xd6", "O")
+	str = str.replace(u"\xfc", "u")
+	str = str.replace(u"\xdc", "U")
+	str = str.replace(u"\xdf", "ss")
+	return str
+
+link = 'http://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=3865001&date=1346623200'
+ical = open("tai10abc.ics", "w")
+#uncomment to use a proxy
+#proxies = {'http': 'http://PROXY_URL:PROXY_PORT'}
+ical.write("BEGIN:VCALENDAR\n")
+ical.write("VERSION:1.0\n")
+
+for weeks in range(12):
+	#html = urllib.urlopen(link, proxies=proxies)
+	html = urllib.urlopen(link)
+	soup = BeautifulSoup(html)
+	weekdays = soup.find_all('ul')
+
+	for weekday in weekdays:
+		month = weekday.li.text[-2:]
+		day = weekday.li.text[-5:]
+		day = day[:2]
+		date = "2012%s%s" % (month, day)
+		for appointment in weekday.find_all('li'):
+			if len(appointment)>1:
+				ical.write("BEGIN:VEVENT\n")
+				properties = appointment.find_all('div')
+				starttime = properties[0].text[:2] + properties[0].text[:5][-2:]
+				endtime = properties[0].text[:8][-2:] + properties[0].text[:11][-2:]
+				description = properties[1].text
+				location = properties[2].text
+				
+				location = convertstring(location)
+				description = convertstring(description)
+				
+				ical.write("LOCATION:%s\n" % (location))
+				ical.write("SUMMARY:%s\n" % (description))
+				ical.write("DESCRIPTION:%s\n" % (description))
+				ical.write("DTSTART:%s\n" % (date + "T" + starttime + "00"))
+				ical.write("DTEND:%s\n" % (date + "T" + endtime + "00"))
+				ical.write("END:VEVENT\n")
+				
+	link = 'http://vorlesungsplan.dhbw-mannheim.de/' + soup.find_all('a')[1].get('href')
+
+ical.write("END:VCALENDAR\n")			
+ical.close()
